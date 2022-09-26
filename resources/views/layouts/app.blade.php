1b91d0c6d644baac0182db42d1ffe31e0a34954d
Krishanu <krishanu.info@gmail.com>
2022-09-27 02:11:27 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,13 +46,12 @@
                 height: 70px;
                 position: absolute;
                 animation: spin 1s linear infinite;
-
                 @else
+                position: fixed;
                 width: 100%;
                 height: 100%;
-                position: fixed;
-                background-image: url('{{$preloaderPath}}') 50% 50% no-repeat rgb(249,249,249);
                 z-index: 9999;
+                background: url('{{$preloaderPath}}') 50% 50% no-repeat rgb(249,249,249);
                 @endif
                 margin: auto;
                 top: 0;
